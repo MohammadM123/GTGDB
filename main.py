48f93d958c8eb94cff8368c8380c3d97f6bb38de
M Mehdi <mm12mehdi@gmail.com>
2026-04-14 10:59:17 +1000

    login webpage

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, session, redirect
 import db
 
 app = Flask(__name__)
@@ -10,6 +10,34 @@ app.secret_key = "gtg"
 def Home():
     guessData = db.GetAllGuesses()
     return render_template("index.html", guesses=guessData)
+
+
+@app.route("/login", methods=["GET", "POST"])
+def Login():
+
+    # They sent us data, get the username and password
+    # then check if their details are correct.
+    if request.method == "POST":
+        username = request.form['username']
+        password = request.form['password']
+
+        # Did they provide good details
+        user = db.CheckLogin(username, password)
+        if user:
+            # Yes! Save their username and id then
+            session['id'] = user['id']
+            session['username'] = username
+
+            # Send them back to the homepage
+            return redirect("/")
+
+    return render_template("login.html")
+
+
+@app.route("/logout")
+def Logout():
+    session.clear()
+    return redirect("/")
 
 
 app.run(debug=True, port=5000)
